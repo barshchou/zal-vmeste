50b4b98fbe40e2b7ce74306925797673f013f8c1
Viachaslau Barshchou <barshchou@kvikna.com>
2026-08-14 02:04:10 +0300

    db url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Подключите Supabase: см. SUPABASE.md
 window.ZAL_CONFIG = {
-  supabaseUrl: "https://plhdjppuytjhwnqgtyej.supabase.co/rest/v1/",
+  supabaseUrl: "https://plhdjppuytjhwnqgtyej.supabase.co",
   supabaseAnonKey: "sb_publishable_xQOEXvFTnechkWMwQYB8Vg_k3QRmb50"
 };
